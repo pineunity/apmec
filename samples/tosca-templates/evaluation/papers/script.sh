fe4bb5d9c96f09cf5bd3df1fa514edf39f6ab842
Tung Doan <doantungbk.203@gmail.com>
2018-07-23 17:56:31 +0200

    Temp update

---
+++ samples/tosca-templates/evaluation/papers/script.sh
@@ -0,0 +1,7 @@
+# Things to do:
+# Create a loop to:
+# Call requests function
+# Afterwards, the samples are created. Then execute 2 approaches:
+# For coop, call only apmec api to create mes
+# For sepa, call apmec api to create mea and call tacker api to create NSs
+
