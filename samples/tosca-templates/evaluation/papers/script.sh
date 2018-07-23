2ce3a9c5b8c11a7cd0c9bd3712c4a3c590c817dc
Tung Doan <doantungbk.203@gmail.com>
2018-07-23 18:15:32 +0200

    Temp update operation

--- samples/tosca-templates/evaluation/papers/script.sh
+++ samples/tosca-templates/evaluation/papers/script.sh
@@ -5,3 +5,31 @@
 # For coop, call only apmec api to create mes
 # For sepa, call apmec api to create mea and call tacker api to create NSs
 
+COUNT=2
+
+
+
+# call source openrc admin admin
+
+count=1
+while [ $count -le $COUNT ]
+
+do
+  # call request function
+  sudo python requests.py
+  count=$(( $count+1 ))
+  mes_name="mes"
+  mes_name+=$count
+  ns_name="ns"
+  ns_name+=$count
+  mea_name="mea"
+  mea_name+=$count
+  # initiate the mes using apmec api
+  apmec mes-create --mesd-template sepa-mesd.yaml $mes_name &
+
+  # Initiate the nss and mea using tacker api and apmec api
+  apmec mea-create --mead_template sepa-mead.yaml $mea_name &
+  tacker ns-create --nsd_template sepa-nsd.yaml $ns_name &
+
+
+
