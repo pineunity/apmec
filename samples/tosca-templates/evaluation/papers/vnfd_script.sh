3fa15147c2ebe53d5b458eaacf457709916cc197
Tung Doan <doantungbk.203@gmail.com>
2018-07-24 13:09:32 +0200

    Minor

--- samples/tosca-templates/evaluation/papers/vnfd_script.sh
+++ samples/tosca-templates/evaluation/papers/vnfd_script.sh
@@ -1,8 +1,16 @@
 
 # call source openrc admin admin
 ostack=/home/ostack/tung-apmec/apmec-eval/devstack
+home=/home/ostack
 edge1_dir=/opt/stack/apmec/samples/tosca-templates/evaluation/papers/edge1
 edge2_dir=/opt/stack/apmec/samples/tosca-templates/evaluation/papers/edge2
+
+cd $home
+
+glance image-create --name "ubuntu-xenial" --disk-format qcow2 --container-format bare --min-disk=3 --visibility public --file xenial-server-cloudimg-amd64-disk1.img --progress
+
+openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
+
 
 vnfd_edge1=("vnfd11" "vnfd12" "vnfd13" "vnfd21" "vnfd22" "vnfd23" "vnfd31" "vnfd32" "vnfd33" "vnfd41" "vnfd42" "vnfd43" "vnfd51" "vnfd52" "vnfd53" "vnfd61" "vnfd62" "vnfd63")
 
