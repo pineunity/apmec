b89a251daa3082471c917560f2c03db0699570a6
Tung Doan <doantungbk.203@gmail.com>
2018-07-24 13:47:02 +0200

    Minor

--- samples/tosca-templates/evaluation/papers/vnfd_script.sh
+++ samples/tosca-templates/evaluation/papers/vnfd_script.sh
@@ -5,6 +5,14 @@ home=/home/ostack
 edge1_dir=/opt/stack/apmec/samples/tosca-templates/evaluation/papers/edge1
 edge2_dir=/opt/stack/apmec/samples/tosca-templates/evaluation/papers/edge2
 
+vnfd_edge1=("vnfd11" "vnfd12" "vnfd13" "vnfd21" "vnfd22" "vnfd23" "vnfd31" "vnfd32" "vnfd33" "vnfd41" "vnfd42" "vnfd43" "vnfd51" "vnfd52" "vnfd53" "vnfd61" "vnfd62" "vnfd63")
+
+
+vnfd_edge2=("vnfd11-edge2" "vnfd12-edge2" "vnfd13-edge2" "vnfd21-edge2" "vnfd22-edge2" "vnfd23-edge2" "vnfd31-edge2" "vnfd32-edge2" "vnfd33-edge2" "vnfd41-edge2" "vnfd42-edge2" "vnfd43-edge2" "vnfd51-edge2" "vnfd52-edge2" "vnfd53-edge2" "vnfd61-edge2" "vnfd62-edge2" "vnfd63-edge2")
+
+cd $ostack
+source userrc_early
+
 cd $home
 
 sudo nova-manage cell_v2 discover_hosts
@@ -12,16 +20,6 @@ sudo nova-manage cell_v2 discover_hosts
 glance image-create --name "ubuntu-xenial" --disk-format qcow2 --container-format bare --min-disk=3 --visibility public --file xenial-server-cloudimg-amd64-disk1.img --progress
 
 openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
-
-
-vnfd_edge1=("vnfd11" "vnfd12" "vnfd13" "vnfd21" "vnfd22" "vnfd23" "vnfd31" "vnfd32" "vnfd33" "vnfd41" "vnfd42" "vnfd43" "vnfd51" "vnfd52" "vnfd53" "vnfd61" "vnfd62" "vnfd63")
-
-
-vnfd_edge2=("vnfd11-edge2" "vnfd12-edge2" "vnfd13-edge2" "vnfd21-edge2" "vnfd22-edge2" "vnfd23-edge2" "vnfd31-edge2" "vnfd32-edge2" "vnfd33-edge2" "vnfd41-edge2" "vnfd42-edge2" "vnfd43-edge2" "vnfd51-edge2" "vnfd52-edge2" "vnfd53-edge2" "vnfd61-edge2" "vnfd62-edge2" "vnfd63-edge2")
-
-cd $ostack
-source openrc admin admin
-cd $sample_dir
 
 count=0
 for sample in $edge1_dir/*
