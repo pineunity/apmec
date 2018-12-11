73c6ae02c057924e095d21c1970577bf834634dd
Tung Doan <doantungbk.203@gmail.com>
2018-12-11 10:33:34 +0100

    Minor update for evaluation

---
+++ papers/provisioning_time.sh
@@ -0,0 +1,45 @@
+# Things to do:
+# Create a loop to:
+# Call requests function
+# Afterwards, the samples are created. Then execute 2 approaches:
+# For coop, call only apmec api to create mes
+# For sepa, call apmec api to create mea and call tacker api to create NSs
+
+
+COUNT=50
+
+
+# call source openrc admin admin
+ostack=/home/ostack/tung-apmec/apmec-eval/devstack
+sample_dir=/opt/stack/apmec/samples/tosca-templates/evaluation/papers
+
+cd $ostack
+source openrc admin admin
+cd $sample_dir
+
+count=1
+while [ $count -le $COUNT ]
+
+do
+  # call request function
+  sudo python requests.py
+  mes_name="mes"
+  mes_name+=$count
+  ns_name="ns"
+  ns_name+=$count
+  mea_name="mea"
+  mea_name+=$count
+  # determine the execution time
+  starting_time = $(date +%s%N)
+  apmec mes-create --mesd-template $sample_dir/coop-mesd.yaml $mes_name
+  exec_time = $((($(date +%s%N) - $starting_time)/1000000))
+  echo $exec_time
+  # Initiate the nss and mea using tacker api and apmec api
+  #apmec mea-create --mead-template $sample_dir/sepa-mead.yaml $mea_name &
+  #tacker ns-create --nsd-template $sample_dir/sepa-nsd.yaml $ns_name &
+
+  count=$(( $count+1 ))
+done
+
+exit 1
+
