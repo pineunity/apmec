8dde39f5f9b4197a6202331055933d99cf71426e
Tung Doan <doantungbk.203@gmail.com>
2018-12-14 17:24:07 +0100

    Update the provisioning time func

--- papers/pros_time.sh
+++ papers/pros_time.sh
@@ -1,12 +1,15 @@
 
+mes_status="PEND_CREATE"
 
-while [ "$status"  != "ACTIVE" ]
-
+while [ "$mes_status"  != "ACTIVE" ]
 
 do
+mes_status=$(apmec mes-show $1 | grep status | awk '{print $4}')
 
+if [[ "$mes_status" == "ACTIVE"  ]]; then
+   echo $mes_status
+fi
 
 done
-
 
 exit 1
