b085a729ed9b0ccf754f1fab89f09d212d040411
Tung Doan <doantungbk.203@gmail.com>
2019-01-23 18:24:53 +0100

    Update chain_pros with flexibility

--- papers/chain_pros.sh
+++ papers/chain_pros.sh
@@ -53,42 +53,55 @@ neutron port-chain-update pc1 --port-pair-group pp2
 
 # get vnf_ids
 
-ns_nname='ns1'
-
+ns_name='ns1'
 
 vnf_ids=$(tacker ns-show $ns_name | grep -w vnf_ids | awk -F'[][]' '{print $2, $4, $6}')
 
-pp_list=""
+ppg_list=""
 
 for vnf_id in $vnf_ids; do
     #echo $vnf_id
+    pp_list=""
     eval vnf_id=$vnf_id
     tacker vnf-resource-list  $vnf_id
     cp_names=$(tacker vnf-resource-list $vnf_id | grep CP | awk '{print $2}')
     # create port-pair-group here
     for cp_name in $cp_names; do
        cp_id=$(tacker vnf-resource-list $vnf_id | grep $cp_name | awk '{print $4}')
-       #echo $cp_id
-       pp_name=$cp_name"-"$vnf_id
+       echo "Create port pair.."
+       pp_name="pp-"$cp_name"-"$vnf_id
        pp_list+=$pp_name" "
        neutron port-pair-create $pp_name --ingress $cp_id --egress $cp_id
     done
     # change the ppq since it is duplicated between NSs
     # should be attched to "ns1"
-    neutron port-pair-group-create $vnf_id --port-pairs $pp_list
+    ppg_name="ppg-"$vnf_id
+    ppg_list+=$ppg_name" "
+
+    echo "Create port pair group..."
+    ppg_creation="neutron port-pair-group-create "
+    prefix=" --port-pair"
+    params=""
+    for pp_name in $pp_list; do
+        params+=$prefix" "$pp_name
+    done
+    ppg_creation+=$ppg_name$params
+    eval $ppg_creation
 done
 
+echo $ppg_list
 
-pc_creation="neutron port-chain-create"
+echo "Create port chain..."
+#neutron port-chain-create pc1 --port-pair-groups $ppg_list
+
+pc_creation="neutron port-chain-create "
 pc_name="pc1"
-prefix="--port-pair-group"
+prefix=" --port-pair-group"
 params=""
-
-for ppg_name in $pp_list; do
-    param+=$prefix" "$ppg_name
-
-pc_creation+=$pc_name" "$pc_name" "$params
-
+for ppg_name in $ppg_list; do
+    params+=$prefix" "$ppg_name
+done
+pc_creation+=$pc_name$params
 eval $pc_creation
 
 
