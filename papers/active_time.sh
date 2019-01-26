18a8c22177dd1f470ff3456f4d9d83beb071add7
Tung Doan <doantungbk.203@gmail.com>
2019-01-26 16:16:34 +0100

    Minor update

--- papers/active_time.sh
+++ papers/active_time.sh
@@ -16,3 +16,37 @@ done
 echo "Done"
 
 exit 1
+
+
+############################## Test ################################
+
+all_check="False"
+
+crd_check='False'
+
+newly_crdNS=''
+
+while [ "$check" == "False" ]
+
+do
+mes_status=$(apmec mes-list | grep meso | awk '{print $8}')
+
+if [[ "$mes_status" != *"PENDING"*  ]]; then
+#echo "Right"
+check="True"
+fi
+
+if [[ "crd_check" == "False"  ]]; then
+
+if [[ "$mes_status" == *"PENDING_CREATE"*  ]]; then
+crd_check="True"
+fi
+
+fi
+
+
+done
+
+echo "Done"
+
+exit 1
